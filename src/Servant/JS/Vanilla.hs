865e2ec264599e328f0ce86e7dfbb8684a75bea0
Antoni Silvestre Padrós <antoni.silvestre@gmail.com>
2018-11-27 23:48:37 +0100

    fix

--- src/Servant/JS/Vanilla.hs
+++ src/Servant/JS/Vanilla.hs
@@ -36,7 +36,7 @@ generateVanillaJSWith opts req = "\n" <>
  <> "  xhr.open('" <> decodeUtf8 method <> "', " <> url <> ", true);\n"
  <>    reqheaders
  <> "  xhr.setRequestHeader('Accept', 'application/json');\n"
- <> (if isJust (req ^. reqBody) && (req ^. reqBodyContentType == ReqBodyJSON)  then "  xhr.setRequestHeader('Content-Type', 'application/json');\n" else "")
+ <> (if isJust (req ^. reqBody) && (req ^. _reqBodyContentType == ReqBodyJSON)  then "  xhr.setRequestHeader('Content-Type', 'application/json');\n" else "")
  <> "  xhr.onreadystatechange = function () {\n"
  <> "    var res = null;\n"
  <> "    if (xhr.readyState === 4) {\n"
@@ -82,7 +82,7 @@ generateVanillaJSWith opts req = "\n" <>
 
         dataBody =
           if isJust (req ^. reqBody)
-            then if (req ^. reqBodyContentType == ReqBodyJSON) then "JSON.stringify(body)" else "body"
+            then if (req ^. _reqBodyContentType == ReqBodyJSON) then "JSON.stringify(body)" else "body"
             else "null"
 
 
